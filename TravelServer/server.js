faa670372f795c48f379a3930434acd535bc1a4f
visrahane <rahane@usc.edu>
2018-03-23 18:50:58 -0700

    add cors funtionality to the server

--- TravelServer/server.js
+++ TravelServer/server.js
@@ -1,10 +1,13 @@
+var cors = require('cors');
 var express=require("express"),
 app=express(),
 port = process.env.PORT || 8090,
 bodyParser = require('body-parser');
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 
 var routes = require('./routes/travelRoutes'); //importing route
