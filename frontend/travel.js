be4cba2191017a8c4b73965f4c0989255beb253f
visrahane <rahane@usc.edu>
2018-03-25 18:22:03 -0700

    implement validation + implement prev button and store pages

--- frontend/travel.js
+++ frontend/travel.js
@@ -1,89 +1,117 @@
-var app=angular.module("loadApp", []);
-app.controller("locController",function($scope,$http){
-    $scope.nextPageUrl={};
+var app = angular.module("loadApp", []);
+app.controller("locController", function ($scope, $http) {
+    $scope.nextPageUrl = {};
+    $scope.pages = [];
+    var currentPage = 0;
     $http({
-        method:"GET",
-        url:"http://ip-api.com/json"
-    }).then(function mySuccess(response){
-       // var locationJson=JSON.parse(response);
-        latitude=response.data.lat;
-        longitude=response.data.lon;
-        $scope.disableSearchBtn=false;
+        method: "GET",
+        url: "http://ip-api.com/json"
+    }).then(function mySuccess(response) {
+        // var locationJson=JSON.parse(response);
+        latitude = response.data.lat;
+        longitude = response.data.lon;
+
         console.log(response.data.lon);
         console.log(response.data.lat);
-    },function myError(response){
-        console.log("Error:",response);
+    }, function myError(response) {
+        console.log("Error:", response);
     });
 
-    $scope.submitFormData=function(){
-        if(!$scope.distance){
-            $scope.distance=10;
+    $scope.submitFormData = function () {
+        if (!$scope.distance) {
+            $scope.distance = 10;
         }
         var location;
-        if(!$scope.location){
-            location="here";
-        }else{
-            location=$scope.location;
+        if (!$scope.location) {
+            location = "here";
+        } else {
+            location = $scope.location;
         }
-        console.log("location",location);
-        if(autocomplete.getPlace()){
-            $scope.location=autocomplete.getPlace().formatted_address;
+        console.log("location", location);
+        if (autocomplete.getPlace()) {
+            $scope.location = autocomplete.getPlace().formatted_address;
         }
         $http({
-            method:"GET",
-            url:"http://webtechtravel-env.us-east-2.elasticbeanstalk.com/searchResults",
-            params:{category:$scope.category,
-                distance:$scope.distance,
-                keyword:$scope.keyword,
-                latitude:latitude,
-                longitude:longitude,
-                location:location
+            method: "GET",
+            url: "http://webtechtravel-env.us-east-2.elasticbeanstalk.com/searchResults",
+            params: {
+                category: $scope.category,
+                distance: $scope.distance,
+                keyword: $scope.keyword,
+                latitude: latitude,
+                longitude: longitude,
+                location: location
             }
-        }).then(function mySuccess(response){
-           // var locationJson=JSON.parse(response);
+        }).then(function mySuccess(response) {
+            // var locationJson=JSON.parse(response);
             console.log(response.data);
-            $scope.results = response.data.results;
+            $scope.pages[currentPage++] = $scope.results = response.data.results;
             toggleVisibility("detailsBtnId");
-            if(response.data.next_page_token!=null && response.data.next_page_token!=""){
+            if (response.data.next_page_token != null && response.data.next_page_token != "") {
                 //set nextPageToken in nextBtn 
                 toggleVisibility("nextBtn");
-                $scope.nextPageUrl=response.data.next_page_token;
+                $scope.nextPageUrl = response.data.next_page_token;
                 //and button should call api when clicked with param nextPageToken
 
             }
-        },function myError(response){
-            console.log("Error:",response);
+        }, function myError(response) {
+            console.log("Error:", response);
         });
     };
-    $scope.fetchNext=function (){
+    $scope.fetchPrev = function () {
+        $scope.results = $scope.pages[--currentPage];
+        if (currentPage == 0) {
+            toggleVisibility("prevBtn");
+            toggleVisibility("nextBtn");
+        }
+
+
+    }
+    $scope.fetchNext = function () {
         toggleVisibility("nextBtn");
-        console.log($scope.nextPageUrl);
-        $http({
-            method:"GET",
-            url:"http://webtechtravel-env.us-east-2.elasticbeanstalk.com/nextSearchResults",
-            params:{nextPageToken:$scope.nextPageUrl
+        toggleVisibility("prevBtn");
+        if (!$scope.pages[currentPage]) {
+            getNextPageFromServer();
+        } else {
+            $scope.results = $scope.pages[++currentPage];
+            if (currentPage == $scope.pages.length) {
+                currentPage--;
             }
-        }).then(function mySuccess(response){
-           // var locationJson=JSON.parse(response);
-            console.log(response.data);
-            $scope.results = response.data.results;
-            if(response.data.next_page_token!=null && response.data.next_page_token!=""){
+        }
+
+
+    }
+    function getNextPageFromServer() {
+        $http({
+            method: "GET",
+            url: "http://webtechtravel-env.us-east-2.elasticbeanstalk.com/nextSearchResults",
+            params: {
+                nextPageToken: $scope.nextPageUrl
+            }
+        }).then(function mySuccess(response) {
+            // var locationJson=JSON.parse(response);            
+            $scope.pages[currentPage] = $scope.results = response.data.results;
+            if (response.data.next_page_token != null && response.data.next_page_token != "") {
                 //set nextPageToken in nextBtn 
                 toggleVisibility("nextBtn");
-                $scope.nextPageUrl=response.data.next_page_token;
-                //and button should call api when clicked with param nextPageToken
+                $scope.nextPageUrl = response.data.next_page_token;
+                currentPage++;
 
             }
-        },function myError(response){
-            console.log("Error:",response);
+        }, function myError(response) {
+            console.log("Error:", response);
         });
-        
     }
-    
     function toggleVisibility(divId) {
         $("#" + divId).toggle();
     }
-    
+
+    //form valid
+    /*if($scope.searchForm.$valid) {
+        console.log("valid enable");
+        $scope.disableSearchBtn=false;
+    }*/
+
 });
 
 var autocomplete;
@@ -113,4 +141,3 @@ function geolocate() {
         });
     }
 }
-
